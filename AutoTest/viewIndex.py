fe0707f32b65e8670e64fc0ee094460bfd783951
ucolourful <506231730@qq.com>
2018-11-14 16:58:56 +0800

    Update viewIndex.py
    [提交原因]: 集成layui
    [提交时间]: 2018年11月14日16:55:44
    [下次提交]: 设计主页

--- AutoTest/viewIndex.py
+++ AutoTest/viewIndex.py
@@ -1,6 +1,15 @@
-# coding = utf-8
+# coding=utf-8
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def testDO(request):
+    # TODO 传入python代码，将其写入uuid的py文件中，然后执行调用
+    return HttpResponse(json.dumps({"status": 1, "msg": request.POST["param"]}), content_type="application/json")
+    # return render(request, "index.html")
