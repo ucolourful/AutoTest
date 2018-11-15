a69fadd61df284599573d5166f04a3663110629c
ucolourful <506231730@qq.com>
2018-11-15 11:38:26 +0800

    Update atIndex.js
    [提交原因]: 备份修改
    [提交时间]: 2018年11月14日16:55:44
    [下次提交]: 设计主页

--- static/js/atIndex.js
+++ static/js/atIndex.js
@@ -1,2 +1,27 @@
-var editor = ace.edit('editor');
-editor.setTheme("ace/theme/tomorrow");
+layui.use(['element','jquery','layer'], function() {
+    var element = layui.element;
+    var layer = layui.layer
+    var $ = layui.jquery
+
+    var editor = ace.edit('editor',{
+        theme: "ace/theme/dracula",
+        mode: "ace/mode/python",
+        autoScrollEditorIntoView: true,
+        maxLines: 30,
+        minLines: 30
+    });
+
+    $("#test").on("click",function(){
+//        $(".layui-header").hidden();
+    });
+
+//    $("#test").on("click",function() {
+//        $.ajax({
+//            type: "POST",
+//            url: "/testDO",
+//            data: { param: editor.getValue() }
+//        }).done(function(ret){
+//            console.log(ret.msg);
+//        });
+//    });
+});
