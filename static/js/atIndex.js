8054e01d73af13c6591c438c7ce4b19a05c6dbd2
ucolourful <506231730@qq.com>
2018-11-20 18:10:18 +0800

    Update atIndex.js
    【提交原因】：完成部分导航栏
    【提交时间】：2018年11月20日18:02:00
    【下次提交】：完成导航栏

--- static/js/atIndex.js
+++ static/js/atIndex.js
@@ -1,12 +1,49 @@
-layui.use(['element', 'jquery', 'layer'], function () {
-    var element = layui.element;
+layui.use(['jquery', 'layer'], function () {
     var layer = layui.layer;
     var $ = layui.jquery;
     var sideDisplay = "0"
 
-    $("#test").on("mousedown", function (event) {
-        layer.alert(event.button);
+    // 监听 顶部右方导航按钮事件
+    $(".header-right").on("click", function(event) {
+        menuClose();
+        if ( $("#"+ this.id +" .header-more").css("border-top-style") != "none" ) {
+            menuOpen(this.id);
+            return false;
+        }
+        menuClose();
     });
+
+    // 关闭menu
+    $(document).on("click", function(event){
+        menuClose();
+        return false;
+    });
+
+    function menuOpen(id) {
+        $("#" + id + " .header-more").css("margin-top","-3px");
+        $("#" + id + " .header-more").css("border-top-style","none");
+        $("#" + id + " .header-more").css("border-bottom-style","solid");
+        $("#" + id + " .header-more").css("border-bottom-color","rgba(255,255,255,.7)");
+        $("#" + id + " .menu-more").css("display","block");
+        $("#" + id + " .menu-more").css("background-color","rgba(0,0,0,.3)");
+        $("#" + id).css("border-bottom","3px solid yellow");
+        $("#" + id).css("color","white");
+    };
+
+    function menuClose() {
+        $(".header-more").css("margin-top","-3px");
+        $(".header-more").css("border-top-style","solid");
+        $(".header-more").css("border-bottom-style","none");
+        $(".header-more").css("border-top-color","rgba(255,255,255,.7)");
+        $(".menu-more").css("display","none");
+        $(".menu-more").css("background-color","rgba(0,0,0,.3)");
+        $(".header-right").css("border-bottom","none");
+        $(".header-right").css("color","rgba(255,255,255,.7)");
+    };
+
+//    $("#test").on("mousedown", function (event) {
+//        layer.alert(event.button);
+//    });
 
     // var editor = ace.edit('editor', {
     //     theme: "ace/theme/dracula",
