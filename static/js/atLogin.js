a157f18069042d6c2c509899e15b0d033d20a35a
ucolourful <506231730@qq.com>
2018-11-29 21:03:22 +0800

    Update atLogin.js
    【提交原因】：数据库设计
    【提交时间】：2018年11月29日20:58:53
    【下次提交】：测试用例展开

--- static/js/atLogin.js
+++ static/js/atLogin.js
@@ -1,32 +1,47 @@
 //AT 登录页 JS
-layui.use(['element','layer','form','jquery'], function () {
-    var element = layui.element;
-    var form = layui.form;
+layui.use(['layer','jquery'], function () {
     layer = layui.layer;
     $ = layui.jquery
 
-    //自定义验证规则
-    form.verify({
-        title: function (value) {
-            if (value.length < 5) {
-                return '用户名也太短了吧';
-            }
-        }
-        , pass: [/(.+){6,12}$/, '密码必须6到12位']
+    // 监听登录按钮
+    $("#at-form-item-submit").on("click",function(event){
+        // 验证用户名密码
+        if ( $.trim($("#username").val()) === "" ) {
+            layer.msg("请输出用户名");
+            return false;
+        };
+        if ( $.trim($("#password").val()) === "" ) {
+            layer.msg("请输入密码");
+            return false;
+        };
+
+        // ajax 异步验证用户名密码
+        $("#at-form-item-submit").attr("disabled","disabled");
+        $("#at-form-item-submit").css("background-color", "grey");
+        $.ajax({
+            type: "POST",
+            url: "/doLogin",
+            data: { username: $.trim($("#username").val()), password: $.trim($("#password").val()) }
+        }).done(function(res){
+            layer.msg(res.msg);
+            if ( res.status === 0 ){
+                setTimeout("window.location.href='/index'",2000)
+            } else {
+                $("#at-form-item-submit").removeAttr("disabled");
+                $("#at-form-item-submit").css("background-color", "#009688");
+            };
+        });
     });
 
-    //监听提交
-    form.on('submit(*)', function (data) {
-        $.post("/userLogin",data.field,function(ret){
-            if ( ret.status == 0 ){
-                layer.msg(ret.msg)
-                setTimeout("window.location.href='/index'",2000)
-            }
-            if ( ret.status == 1 ){
-                layer.msg(ret.msg)
-            }
-        })
-//        layer.msg(JSON.stringify(data.field))
-        return false
+    // 监听重置按钮
+    $("#at-form-item-reset").on("click",function(event){
+        $(".at-form-item-input").val("");
+    });
+
+    // 监听回车键，触发登录事件
+    $(document).keyup(function(event) {
+        if ( event.keyCode === 13) {
+            $("#at-form-item-submit").trigger("click");
+        };
     });
 });
